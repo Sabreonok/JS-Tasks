cc8f7222456616ec283a7f03210d16fd1a94617c
Sabreonok <105870708+Sabreonok@users.noreply.github.com>
2022-09-18 11:24:45 +0300

    JS-Tasks

---
+++ scripts.js
@@ -0,0 +1,7 @@
+let user = prompt('Ваше имя');
+alert('Привет, ' + user);
+
+
+let num1 = prompt('Число');
+let num2 = prompt('Степень');
+console.log (num1 ** num2);
---
+++ scripts2.js
@@ -0,0 +1,3 @@
+let num1 = prompt('Число');
+let num2 = prompt('Степень');
+console.log (num1 ** num2);
